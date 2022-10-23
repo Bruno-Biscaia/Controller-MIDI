6a9f3649ca7f5d38a47187094e6c35fd62ad1fec
Bruno Biscaia <bruno.biscaia@hotmail.com>
2022-10-23 10:23:46 -0300

    finalizar projeto

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-function tocaSom (idAudio) {
-    document.querySelector(idAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+        
+        if(elemento != null && elemento.localName === 'audio') {
+            elemento.play();            
+        } 
+        else {
+            console.log('Elemento não encontrado ou seletor inválido!');
+        }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador =0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSom;    
-    contador += 1
-    console.log()
+    tecla.onclick = function () {
+        tocaSom (idAudio);        
+    }     
+
+    tecla.onkeydown = function (e) {
+        if(e.code === 'Space'|| e.code === 'Enter') {
+            tecla.classList.add('ativa');
+        } 
+    }
+
+    tecla.onkeyup = function (e) {
+        tecla.classList.remove('ativa');
+    }
+
 }
+
+
+
